88446ce500d1872a5a1ea6f9e16d6df9e3410cf8
topdev1030 <mercury427427@gmail.com>
2023-10-23 13:13:52 +0300

    inputtext + button

--- packages/vue-ui/components/Button.vue
+++ packages/vue-ui/components/Button.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="card flex justify-content-center">
-    <InputText id="value" type="text" v-model="value" />
-    <Button type="submit" label="Submit" @click="handleClick" />
+    <!-- <InputText id="value" type="text" v-model="value" />
+    <input type="text" name="value" id="value" v-model="value" />
+    <Button type="submit" label="Submit" @click="handleClick" /> -->
+    <div class="p-inputgroup flex-1">
+      <Button type="submit" label="Submit" @click="handleClick" />
+      <InputText id="value" type="text" v-model="value" placeholder="Value" />
+    </div>
   </div>
 </template>
 
@@ -12,7 +17,7 @@ import Button from 'primevue/button';
 import InputText from 'primevue/inputtext';
 
 export default {
-  components: { Button },
+  components: { Button, InputText },
   setup() {
     const value = ref('');
 
