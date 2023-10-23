b0140508f97b58653c3cc0de2e9c1cc212a594c9
topdev1030 <mercury427427@gmail.com>
2023-10-23 13:01:04 +0300

    inputtext + button

--- packages/vue-ui/components/Button.vue
+++ packages/vue-ui/components/Button.vue
@@ -1,17 +1,29 @@
 <template>
-  <Button label="Submit" @click="handleClick" />
+  <div class="card flex justify-content-center">
+    <InputText id="value" type="text" v-model="value" />
+    <Button type="submit" label="Submit" @click="handleClick" />
+  </div>
 </template>
 
 <script lang="ts">
-// import Vue from "vue";
+import { ref } from 'vue';
+
 import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
 
 export default {
   components: { Button },
-  methods: {
-    handleClick: () => {
-      alert("Woooha!")
-    },
+  setup() {
+    const value = ref('');
+
+    const handleClick = () => {
+      alert('Woooha!' + value.value);
+    };
+
+    return {
+      value,
+      handleClick,
+    };
   },
 }
 </script>
